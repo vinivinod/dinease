103e5e354f7dc695a3a8bd028fa418ee237ed4cb
Vini Vinod <vinivinod2024@mca.ajce.in>
2024-05-07 16:11:05 +0530

    commit

--- DineEase/settings.py
+++ DineEase/settings.py
@@ -89,15 +89,16 @@ WSGI_APPLICATION = 'DineEase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.parse("postgres://dineease_user:SghT15dl9NWeDUkvE0Wt9NZrQIzxyKTx@dpg-co448dn79t8c738ugsq0-a.singapore-postgres.render.com/dineease")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse("postgres://dineease_user:SghT15dl9NWeDUkvE0Wt9NZrQIzxyKTx@dpg-co448dn79t8c738ugsq0-a.singapore-postgres.render.com/dineease")
+# }
 
 
 # Password validation
